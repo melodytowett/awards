f5e8be0a0ac8a416dc3ce5bc415dc7f649feca6a
melodytowett <melodytowett99@gmail.com>
2022-04-09 00:32:11 +0300

    add update profile function

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns=[
     path('',views.index,name = 'index'),
     path('post-project/',views.my_projects,name='post-project'),
+    path('profile/',views.update_profile,name='update_profile')
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 import re
 from django.shortcuts import redirect, render
 from.models import Projects,Profile
-from projects.forms import ProjectForm
+from projects.forms import ProfileForm, ProjectForm
 
 # Create your views here.
 
@@ -22,3 +22,16 @@ def my_projects(request):
     else:
         post_form = ProjectForm()
     return render(request,'all-projects/post-project.html',{"post_form":post_form})
+
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST,request.FILES)
+        if profile_form.is_valid():
+            profile  = profile_form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+            return redirect('update_profile')
+    else:
+        profile_form = ProfileForm()
+    return render(request,'all-projects/profile.html',{"profile_form":profile_form})
