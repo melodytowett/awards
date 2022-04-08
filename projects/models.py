50040e059814119e1addbf6f1c25d60f8a5d1b4c
melodytowett <melodytowett99@gmail.com>
2022-04-08 11:45:54 +0300

    create models and migrate

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
+from django.db import models
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+    description = models.CharField(max_length=300)
+    link = models.TextField(max_length=200)
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    user_bio = models.TextField(blank=True)
+    Projects = models.ForeignKey(Projects,on_delete=models.CASCADE)
+    contact = models.TextField(blank=True)
